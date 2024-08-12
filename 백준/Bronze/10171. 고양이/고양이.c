cec9b7410e37d4ce9d4cd53c91597ccd317958e1
s00ya <highlight8990@naver.com>
2024-08-12 15:44:21 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    char a[] = "\\    /\\";
+    printf("%s\n", a);
+    char b[] = " )  ( ')";
+    printf("%s\n", b);
+    char c[] = "(  /  )";
+    printf("%s\n", c);
+    char d[] = " \\(__)|";
+    printf("%s", d);
+    return 0;
+}
