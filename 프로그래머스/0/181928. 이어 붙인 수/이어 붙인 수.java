f26d401951f6beef118b1bfdce3dea97c2dac0e3
s00ya <jjschoho@gmail.com>
2025-01-25 21:26:45 +0900

    [level 0] Title: 이어 붙인 수, Time: 16.89 ms, Memory: 90 MB -BaekjoonHub

---
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int[] num_list) {
+        String a = "";
+        String b = "";
+        
+        for(int i = 0; i < num_list.length; i++) {
+            if(num_list[i] % 2 != 0) {
+               a += num_list[i];
+            } else {
+               b += num_list[i];
+            }
+        } 
+        int c = Integer.parseInt(a) + Integer.parseInt(b);
+        return c;
+    }
+}
