7d30ddd9dd4d7e8086c8589f97503c80df38f590
s00ya <jjschoho@gmail.com>
2025-01-25 21:37:42 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.02 ms, Memory: 92.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        boolean yes = num1 == num2;
+        if(yes) {
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+}
